f0e95c40114bce33c9b816377d0612ea638022af
«Dmitrii <Ambal003@ya.ry>
2022-07-10 02:21:48 +0300

    Сделал первую задачу

---
+++ Task #1/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+        Console.WriteLine("Введите трехзначное число");
+        int num = int.Parse(Console.ReadLine());
+        int n1 = num / 10;
+        int n2 = n1 * 10;
+        int n3 = num - n2;
+        Console.WriteLine("Последняя цифра числа = " + n3);
+    }
+}
