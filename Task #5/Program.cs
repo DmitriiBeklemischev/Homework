8bd4119193bac4c5caf3ae054edeeafa36b61766
«Dmitrii <Ambal003@ya.ry>
2022-07-11 22:45:55 +0300

    Домашнее задание выполнил

--- Task #5/Program.cs
+++ Task #5/Program.cs
@@ -1,17 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
-Console.WriteLine("Введите число > 1 :");
-int num = int.Parse(Console.ReadLine());
-int i = 1;
-if (i >= num)
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+while (number > 1)
 {
-    if (i % 2 == 0)
+    if (number % 2 == 0) 
     {
-    Console.WriteLine(num);
+    Console.WriteLine("Четное число " + number);
     }
-int num = num - 1;
-int i = i + 1;
-}
-else
-{
-   Console.WriteLine("Нет четных чисел");
+    number = number - 1;
 }
